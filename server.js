287c66a37774c118cdc810e3a0645871828bdb2a
sachindaingade <sbdaingade@gmail.com>
2025-07-06 18:31:10 +0530

    code clean

--- server.js
+++ server.js
@@ -44,10 +44,7 @@ app.get('/getDate', (req, res) => {
         localeString: now.toLocaleString(),
         dateString: now.toLocaleDateString(),
         timeString: now.toLocaleTimeString(),
-       // customFormat: `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now.getDate().toString().padStart(2, '0')} ${now.getHours().toString().padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}:${now.getSeconds().toString().padStart(2, '0')}`;
     };
-   // res.json(responseData);
-
     res.status(200).json({ status: responseData});
 });
 
